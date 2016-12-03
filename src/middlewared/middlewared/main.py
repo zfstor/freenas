ca3fcd2448d491686683281cfa81008612309ee0
Vito Oquendo <vito.oquendo@gmail.com>
2016-12-03 15:26:17 +0000

    Update rollbar key

--- src/middlewared/middlewared/main.py
+++ src/middlewared/middlewared/main.py
@@ -269,7 +269,7 @@ class Middleware(object):
 
     def __init_rollbar(self):
         rollbar.init(
-            'caf06383cba14d5893c4f4d0a40c33a9',
+            '1235382317f5413dbf5f04b3e05aed68',
             'production' if 'DEVELOPER_MODE' not in os.environ else 'development'
         )
 
